1d48f813303744bd64d25a631142c8426b81377b
Bibek Pandey <bewakepandey@gmail.com>
2022-08-03 19:19:54 +0545

    limit multi lang sentences to 1500 each

--- concat_treebanks.py
+++ concat_treebanks.py
@@ -28,21 +28,29 @@ args = parser.parse_args()
 treebanks = util.get_ud_treebank_files(args.dataset_dir, args.treebanks)
 train, dev, test = list(zip(*[treebanks[k] for k in treebanks]))
 
+PER_LANG_SENTS = 1500
+
 for treebank, name in zip([train, dev, test], ["train.conllu", "dev.conllu", "test.conllu"]):
+    lang_sent_count = {}
     with open(os.path.join(args.output_dir, name), 'w') as write:
         for t in treebank:
             if not t:
                 continue
             with open(t, 'r') as read:
+                lang = os.path.basename(t).split('-')[0].split('_')[0]
                 if not args.lang_id:
                     shutil.copyfileobj(read, write)
                 else:
                     for line in read:
+                        if lang_sent_count.get(lang, 0) > PER_LANG_SENTS:
+                            break
                         if line != '\n' and not line.startswith('#'):
                             form = line.rstrip('\n').split('\t')
                             #form += ['treebank=' + os.path.basename(t).split('-')[0]]
-                            form += [os.path.basename(t).split('-')[0].split('_')[0]]
+                            form += [lang]
                             write.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(*form))
                         else:
+                            lang_sent_count[lang] = lang_sent_count.get(lang, 0) + 1
                             write.write(line)
         write.close()
+    print(lang_sent_count)
